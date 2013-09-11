b6bc03a99c0cea4ca6ea2e897d68055844374d3a
Nemesis Fixx <joewillrich@gmail.com>
2013-09-11 12:49:42 +0300

    integrate customizations

--- magic/views.py
+++ magic/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from magic.models import Event
+from diary.settings import *
 
 def home(request):
-    return render_to_response('magic/home.html',RequestContext(request,{
-
+    return render_to_response('magic/index.html',RequestContext(request,{
+            'MAGICIAN_NAME' : MAGICIAN_NAME,
+            'title' : HOME_TITLE,
+            'description' : SITE_DESCRIPTION
             }))
